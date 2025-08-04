8f0bd0db66cbfd2448da0d0aedcb58ceb8aad918
bizpect <60975045+bizpect@users.noreply.github.com>
2025-08-04 20:46:29 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/app-ads.txt',      // 경로
+        destination: '/app-ads.txt', // 리디렉션 없이 동일 경로로 서빙
+        permanent: false,            // 리디렉션이 영구적이지 않음
+      },
+    ];
+  },
 };
 
 export default nextConfig;
